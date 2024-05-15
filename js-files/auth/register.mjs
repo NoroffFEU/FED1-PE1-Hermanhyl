93bf67fa3b48be523db9394367ebec78629c5a01
Hermanhyl <hermanhyl@hotmail.com>
2024-05-15 13:20:46 +0200

    made register and login functions

---
+++ js-files/auth/register.mjs
@@ -0,0 +1,57 @@
+import {register_Api} from "../constant.mjs"
+
+import { doFetch } from "../doFetch.mjs";
+
+const registerForm =  document.querySelector('#register-form')
+
+registerForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = event.target[0].value
+    const email = event.target[1].value
+    const password = event.target[2].value
+    registerUser(name, email, password);
+});
+
+async function registerUser(name, email, password) {
+    console.log('register user');
+    await doFetch(register_Api, false, {
+        method: 'POST',
+        body: JSON.stringify({
+            name,
+            email,
+            password
+        }),
+    });
+}
+
+
+
+// import { register_Api } from "../constant.mjs";
+// import { setRegisterFormListener } from "../handlers/register.mjs";
+
+// const action = "/js-files/auth/register.mjs";
+// const method = "post";
+
+// export async function register(profile) {
+//     // const registerUrl = api_Url + action;
+//     const body = JSON.stringify(profile);
+
+//     const response = await fetch(register_Api, {
+//         headers: {
+//             "content-Type": "application/json"
+//         },
+//         method: 'POST',
+//         body: JSON.stringify({
+//             name,
+//             email,
+//             password
+//         })
+//     })
+// }
+
+//     setRegisterFormListener()
+
+//     const result = await response.json()
+//     console.log(result);
+
+
---
+++ js-files/login.mjs
@@ -0,0 +1,32 @@
+import {API_Login_Url} from "../js-files/constant.mjs"
+import { doFetch } from "../js-files/doFetch.mjs";
+
+
+const runPage = () => {
+    const loginForm = document.querySelector('#login-form');
+    loginForm.addEventListener('submit', async (event) => {
+        event.preventDefault() 
+        try {
+            let formData = new FormData(loginForm)
+
+            const response = await doFetch(API_Login_Url, false, {
+                method: 'POST',
+                body: JSON.stringify({
+                    email: formData.get('username'),
+                    password: formData.get('password')
+                })
+            });
+            if(!response) {
+                alert('your email or password is wrong') 
+                return
+            }
+            console.log(response); // Log the entire response object
+            const accessToken = response.data;
+            localStorage.setItem('access-token', JSON.stringify(accessToken));
+        } catch (error) {
+            console.error('Error while logging in:', error);
+        }
+    } )
+}
+
+runPage()
