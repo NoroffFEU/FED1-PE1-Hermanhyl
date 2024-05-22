8b9385b2a1455b7a545208734071130e532392a1
Hermanhyl <hermanhyl@hotmail.com>
2024-05-22 14:34:35 +0200

    added some new constants

--- js-files/constant.mjs
+++ js-files/constant.mjs
@@ -1,5 +1,9 @@
+
+
 const api_Url = 'https://v2.api.noroff.dev/';
 
 export const register_Api = `${api_Url}auth/register`
 export const API_Login_Url = `${api_Url}auth/login`
-export const API_Key_Url = `${api_Url}auth/create-api-key`
+export const API_Key_Url = `${api_Url}auth/create-api-key`  
+export const API_Add_Post_Url = `${api_Url}blog/posts/bilbobolla`
+export const API_Edit_Post = `${api_Url}blog/posts/bilbobolla/`
